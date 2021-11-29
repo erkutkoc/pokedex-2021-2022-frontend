2c926c136a83735f4bca396fe0e42546af1dae70
Erkut Koc <erkut.koc@outlook.com>
2021-11-29 14:28:08 +0100

    add - register component page

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -1,4 +1,5 @@
 import HomePage from "../Pages/HomePage";
+import ProfilPage from "../Pages/ProfilPage";
 import NewPage from "../Pages/NewPage";
 import LoginPage from "../Pages/LoginPage";
 import RegisterPage from "../Pages/RegisterPage";
@@ -10,6 +11,7 @@ const routes = {
   "/new": NewPage,
   "/login": LoginPage,
   "/register": RegisterPage,
+  "/profil": ProfilPage,
   "/logout": Logout
 };
 
