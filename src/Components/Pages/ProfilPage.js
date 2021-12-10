b118e2b5a00161ff73d2dea9e3272210c70696d5
maximeVa <maxime.vanachter@student.vinci.be>
2021-12-10 01:07:05 +0100

    Add - ProfilPage formulaire + fetch get pour aller chercher le user dans le backend ( pas encore fini, ne gère pas encore l'update de ces données)

--- src/Components/Pages/ProfilPage.js
+++ src/Components/Pages/ProfilPage.js
@@ -1,21 +1,127 @@
 import { getSessionObject } from "../../utils/session"; // destructuring assignment ("{}": see MDN for more info ; )
 import { Redirect } from "../Router/Router";
-import Navbar from "../Navbar/Navbar";
-import { setSessionObject } from "../../utils/session";
+
 /**
  * Render the profil page
  */
 
- const ProfilPage = () => { 
-    let userSession = getSessionObject("user");
-    console.log(userSession, "user");
-    if (!userSession) {
-        return Redirect("/login");
-    }
+const ProfilPage = async () => {
+  let userSession = getSessionObject("user");
+  console.log(userSession, "user");
+  if (!userSession) {
+    return Redirect("/login");
+  }
+  const userId = userSession.id;
+  const pageDiv = document.querySelector("main");
 
-    const pageDiv = document.querySelector("main");
-    pageDiv.innerHTML = "Deal with the content of your ProfilPage";
-  };
-  
-  export default ProfilPage;
-  
+  try {
+    const options = {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: user.token,
+      },
+    };
+    const response = await fetch("/api/users/" + userId, options); // fetch return a promise => we wait for the response
+
+    if (!response.ok) {
+      throw new Error(
+        "fetch error : " + response.status + " : " + response.statusText
+      );
+    }
+    const user = await response.json(); // json() returns a promise => we wait for the data
+    console.log("user authenticated", user);
+  } catch (error) {
+    console.error("ProfilPage::error: ", error);
+  }
+
+  let profilPage = `
+    <h5>Profil</h5>
+    <div class="col">
+      <div class="row">
+        <div class="col mb-3">
+          <div class="card">
+            <div class="card-body">
+              <div class="e-profile">
+                <div class="row">
+                  
+                  <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
+                    <div class="text-center text-sm-left mb-2 mb-sm-0">
+                      <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">${userSession.pseudo}</h4>
+                      <div class="mt-2">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <ul class="nav nav-tabs">
+                  <li class="nav-item"><a href="#" class="active nav-link">Settings</a></li>
+                </ul>
+                <div class="tab-content pt-3">
+                  <div class="tab-pane active">
+                    <form class="form" novalidate="">
+                      <div class="row">
+                        <div class="col">
+                          <div class="row">
+                            <div class="col">
+                              <div class="form-group">
+                                <label>Pseudo</label>
+                                <input class="form-control" type="text" name="Pseudo" placeholder="${userSession.pseudo}">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col">
+                              <div class="form-group">
+                                <label>Email</label>
+                                <input class="form-control" type="text" placeholder="${userSession.email}">
+                              </div>
+                            </div>
+                          </div>
+                          <br/>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-12 col-sm-6 mb-3">
+                          <div class="mb-2"><b>Change Password</b></div>
+                          <div class="row">
+                            <div class="col">
+                              <div class="form-group">
+                                <label>Current Password</label>
+                                <input class="form-control" type="password" placeholder="••••••">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col">
+                              <div class="form-group">
+                                <label>New Password</label>
+                                <input class="form-control" type="password" placeholder="••••••">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="row">
+                            <div class="col">
+                              <div class="form-group">
+                                <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
+                                <input class="form-control" type="password" placeholder="••••••"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col d-flex justify-content-end">
+                          <button class="btn btn-primary" type="submit">Save Changes</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div> 
+          </div>
+        </div>`;
+
+  pageDiv.innerHTML = profilPage;
+};
+
+export default ProfilPage;
